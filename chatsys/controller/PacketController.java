da293e9cda0bb901712f05ccf71a0b5242745325
bonicatt <sacha.bonicatto@gmail.com>
2023-11-13 12:02:57 +0100

    pas grand chose

--- chatsys/controller/PacketController.java
+++ chatsys/controller/PacketController.java
@@ -1,8 +1,13 @@
 package chatsys.controller;
 
+import chatsys.User;
 
-public class PacketController {
+public class PacketController implements Controller {
 
     private static final PacketController instance = new PacketController();
+
+    public void initController() {
+
+    }
 
 }
